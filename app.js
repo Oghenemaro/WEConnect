56fc08995f075be1c01bdb12b7d13dee418f19b0
Maro <marowinse@outlook.com>
2018-03-02 23:55:24 +0100

    Update application config file
    Problem: in attempt to read a data passed the body of a request (req.body) error was encountered: application wasn't set to use body-parser's json()
    
    Solution: set application to use body-parser's json()-it was already added just commented out, now readded.

--- app.js
+++ app.js
@@ -1,16 +1,22 @@
 import express from 'express';
 import logger from 'morgan';
-import parser from 'body-parser';
+import bodyParser from 'body-parser';
+
 
 const app = express();
 
 app.use(logger('dev'));
 
-// app.use(parser.json);
-app.use(parser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+const Route = require('./server/routes/routes');
+
+app.use('/', Route);
 
 app.get('*', (req, res) => res.status(200).send({
-    message: 'Welcome to the beginning of nothingness.',
-  }));
+  message: 'Welcome to the beginning of nothingness.',
+}));
+
 
 module.exports = app;

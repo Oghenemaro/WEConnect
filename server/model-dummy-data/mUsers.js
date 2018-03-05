ffbe903e49b055970a65c5c322e612b09f3c7ac4
Maro <marowinse@outlook.com>
2018-03-05 14:14:31 +0100

    Create users dummy data
    here an array of objects with user's properties is created to emmulate working with an actual database

---
+++ server/model-dummy-data/mUsers.js
@@ -0,0 +1,23 @@
+const addUser = [
+  {
+    id: 1,
+    username: 'maro',
+    password: 'mweconnect',
+    firstname: 'Oghenemaro',
+    lastname: 'Onokpise',
+    email: 'nerdmaro@gmail.com',
+    telephone: '08073349770',
+  },
+  {
+    id: 2,
+    username: 'zino',
+    password: 'zweconnect',
+    firstname: 'Ewomazino',
+    lastname: 'Onokpise',
+    email: 'ewomazino@gmail.com',
+    telephone: '08023454722',
+  },
+];
+
+export default addUser;
+

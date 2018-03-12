ddd2fa752dcffab7a475349f5092e1e4f93e8973
Maro <marowinse@outlook.com>
2018-03-12 17:28:04 +0100

    Add controller to create/register a user

--- server/controller-dummy-data/users.js
+++ server/controller-dummy-data/users.js
@@ -2,7 +2,27 @@ import users from '../model-dummy-data/mUsers';
 
 class UsersController {
   static getUsers(req, res) {
-    return res.status(200).json({ Users: users });
+    if (req) {
+      return res.status(200).json({ Users: users });
+    }
+  }
+
+  static createUser(req, res) {
+    if (req.body) {
+      users.push({
+        id: users.length + 1,
+        username: req.body.username,
+        password: req.body.password,
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        email: req.body.email,
+        telephone: req.body.telephone,
+        address: req.body.address
+      });
+      res.status(200).json({ message: 'User Account Created' });
+    } else {
+      return res.status(400).json({ message: 'Request is empty' });
+    }
   }
 }
 

25a6c4d21c2a2f5c5ed6467951ecdcac23251b0d
Maro <marowinse@outlook.com>
2018-03-12 18:23:49 +0100

    Add routes to controller

---
+++ server/routes/routes.js
@@ -0,0 +1,24 @@
+import express from 'express';
+import users from '../controller-dummy-data/users';
+import business from '../controller-dummy-data/business';
+
+const router = express.Router();
+
+router.get('/api', (req, res) => res.status(200).send({
+  message: 'Welcome to WeConnect',
+}));
+
+router.get('/api/v1/auth/users', users.getUsers);
+router.post('/api/v1/auth/signup', users.createUser);
+router.post('/api/v1/auth/signin', users.authenticateUser);
+router.get('/api/v1/businesses', business.getAllBusinesses);
+router.post('/api/v1/businesses/', business.createBusiness);
+router.delete('/api/v1/businesses/:id', business.deleteBusiness);
+router.put('/api/v1/businesses/:id', business.modifyBusiness);
+router.get('/api/v1/businesses/:id', business.getABusiness);
+router.post('/api/v1/businesses/:id/reviews', business.addBusinessReview);
+router.get('/api/v1/business/:id/reviews', business.getABusinessReviews);
+router.get('/api/v1/business?location', business.getAllBusinesses);
+
+
+module.exports = router;

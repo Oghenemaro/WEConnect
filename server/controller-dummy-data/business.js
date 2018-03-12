defe119d951bfdb44c8ce8f21dc18919f6dcbf7b
Maro <marowinse@outlook.com>
2018-03-12 17:51:57 +0100

    modify an existing business

--- server/controller-dummy-data/business.js
+++ server/controller-dummy-data/business.js
@@ -16,6 +16,24 @@ class Business {
       return res.status(400).send({ message: 'A record must be entered' });
     }
   }
+
+  static modifyBusiness(req, res) {
+    if (req.params.id) {
+      for (let i = 0; i <= business.length; i += 1) {
+        if (business[i].id === parseInt(req.params.id, 10)) {
+          business[i].business_name = req.body.business_name;
+          business[i].business_description = req.body.business_description;
+          business[i].business_location = req.body.business_location;
+          business[i].business_category = req.body.business_category;
+          business[i].reviews = req.body.reviews;
+          res.status(200).send({ record: business[i], message: 'Record Updated' });
+        }
+      }
+      res.status(400).send({ message: 'Record not found, please select an existing business' });
+    } else {
+      return res.status(400).send({ message: 'You are not signed in, Please sign in' });
+    }
+  }
 }
 
 export default Business;

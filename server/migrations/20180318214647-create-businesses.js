7e60fd076ceae57182677689ca3a6086ef348239
Maro <marowinse@outlook.com>
2018-03-19 00:13:21 +0100

    create a model for business record

---
+++ server/migrations/20180318214647-create-businesses.js
@@ -0,0 +1,25 @@
+
+
+module.exports = {
+  up: (queryInterface, Sequelize) => queryInterface.createTable('businesses', {
+    id: {
+      allowNull: false,
+      autoIncrement: true,
+      primaryKey: true,
+      type: Sequelize.INTEGER
+    },
+    business_name: {
+      type: Sequelize.STRING,
+      allowNull: false
+    },
+    createdAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    },
+    updatedAt: {
+      allowNull: false,
+      type: Sequelize.DATE
+    }
+  }),
+  down: queryInterface => queryInterface.dropTable('businesses'),
+};
---
+++ server/models/businesses.js
@@ -0,0 +1,50 @@
+
+
+module.exports = (sequelize, DataTypes) => {
+  const businesses = sequelize.define('businesses', {
+    business_name: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    business_description: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    business_address: {
+      type: DataTypes.TEXT,
+      allowNull: false
+    },
+    business_category: {
+      type: DataTypes.STRING,
+      allowNull: false
+    },
+    business_location: {
+      type: DataTypes.STRING,
+      allowNull: false
+    }
+  });
+  businesses.associate = (models) => {
+    businesses.hasMany(models.reviews, {
+      foreignkey: 'id'
+    });
+  };
+  businesses.associate = (models) => {
+    businesses.belongsTo(models.category, {
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    });
+  };
+  businesses.associate = (models) => {
+    businesses.belongsTo(models.locations, {
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    });
+  };
+  businesses.associate = (models) => {
+    businesses.belongsTo(models.users, {
+      onDelete: 'CASCADE',
+      onUpdate: 'CASCADE'
+    });
+  };
+  return businesses;
+};

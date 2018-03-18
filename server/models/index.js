9671bfa03d647c5f6b3b98868743eaa9633be481
Maro <marowinse@outlook.com>
2018-03-18 18:50:21 +0100

    setup connection to database

--- server/models/index.js
+++ server/models/index.js
@@ -1,15 +1,16 @@
+import fs from 'fs';
+import path from 'path';
+import Sequelize from 'sequelize';
+import dotenv from 'dotenv';
+import configLocation from '../config/config.json';
 
-
-const fs = require('fs');
-const path = require('path');
-const Sequelize = require('sequelize');
-
+dotenv.config();
 const basename = path.basename(__filename);
 const env = process.env.NODE_ENV || 'development';
-const config = require(`${__dirname}/..\config\config.js`)[env];
+const config = configLocation[env];
 const db = {};
-let sequelize;
 
+let sequelize;
 if (config.use_env_variable) {
   sequelize = new Sequelize(process.env[config.use_env_variable], config);
 } else {

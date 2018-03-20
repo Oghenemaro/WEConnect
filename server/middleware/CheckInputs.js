92e96b87c9a38df87b07bd0a80d71f7c71182e96
Maro <marowinse@outlook.com>
2018-03-20 19:37:08 +0100

    Create middleware to check user inputs for empty values

--- server/middleware/CheckInputs.js
+++ server/middleware/CheckInputs.js
@@ -15,6 +15,20 @@ class CheckInput {
     }
     next();
   }
+  static checkBusinessFormInput(req, res, next) {
+    const {
+      businessName,
+      businessDescription,
+      businessLocation,
+      businessCategory,
+      reviews
+    } = req.body;
+    const result = (businessName && businessDescription && businessLocation && businessCategory && reviews !== '') ? 1 : 0;
+    if (result === 0) {
+      return res.status(400).send({ Status: 'failed', message: 'All records must be provided, kindly provide all requested information' });
+    }
+    next();
+  }
 }
 
 export default CheckInput;

9748ffd65e24103962c222d321b1c91cda392bf6
Maro <marowinse@outlook.com>
2018-03-21 02:06:10 +0100

    Refactor reviews db model and its migration file

--- server/models/reviews.js
+++ server/models/reviews.js
@@ -5,16 +5,22 @@ module.exports = (sequelize, DataTypes) => {
     review: {
       type: DataTypes.STRING,
       allowNull: false
+    },
+    count: {
+      type: DataTypes.INTEGER,
+      allowNull: false
     }
   });
   reviews.associate = (models) => {
     reviews.belongsTo(models.users, {
+      foreignkey: 'userId',
       onDelete: 'CASCADE',
       onUpdate: 'CASCADE'
     });
   };
   reviews.associate = (models) => {
     reviews.belongsTo(models.businesses, {
+      foreignkey: 'businessId',
       onDelete: 'CASCADE',
       onUpdate: 'CASCADE'
     });

6464a235f530236605f6b6f0d1dbb392879e35b5
Maro <marowinse@outlook.com>
2018-03-23 13:48:16 +0100

    Update to routes referencing dummy data classes

--- server/routes/routes.js
+++ server/routes/routes.js
@@ -1,6 +1,6 @@
 import express from 'express';
-import users from '../controller-dummy-data/users';
-import business from '../controller-dummy-data/business';
+import Users from '../controller-dummy-data/Users';
+import Business from '../controller-dummy-data/Business';
 import CheckInput from '../middleware/CheckInputs';
 
 const router = express.Router();
@@ -9,18 +9,18 @@ router.get('/api', (req, res) => res.status(200).send({
   message: 'Welcome to WeConnect',
 }));
 
-router.get('/api/v1/auth/users', users.getUsers);
-router.post('/api/v1/auth/signup', CheckInput.checkUserFormInput, users.createUser);
-router.post('/api/v1/auth/signin', users.authenticateUser);
-router.get('/api/v1/businesses', business.getAllBusinesses);
-router.post('/api/v1/businesses/', CheckInput.checkBusinessFormInput, business.createBusiness);
-router.delete('/api/v1/businesses/:id', business.deleteBusiness);
-router.put('/api/v1/businesses/:id', CheckInput.checkBusinessFormInput, business.modifyBusiness);
-router.get('/api/v1/businesses/:id', business.getABusiness);
-router.post('/api/v1/businesses/:id/reviews', business.addBusinessReview);
-router.get('/api/v1/business/:id/reviews', business.getABusinessReviews);
-router.get('/api/v1/businesses?location', business.getAllBusinesses);
-router.get('/api/v1/businesses?category', business.getAllBusinesses);
+router.get('/api/v1/auth/users', Users.getUsers);
+router.post('/api/v1/auth/signup', CheckInput.checkUserFormInput, Users.createUser);
+router.post('/api/v1/auth/signin', Users.authenticateUser);
+router.get('/api/v1/businesses', Business.getAllBusinesses);
+router.post('/api/v1/businesses/', CheckInput.checkBusinessFormInput, Business.createBusiness);
+router.delete('/api/v1/businesses/:id', Business.deleteBusiness);
+router.put('/api/v1/businesses/:id', CheckInput.checkBusinessFormInput, Business.modifyBusiness);
+router.get('/api/v1/businesses/:id', Business.getABusiness);
+router.post('/api/v1/businesses/:id/reviews', Business.addBusinessReview);
+router.get('/api/v1/business/:id/reviews', Business.getABusinessReviews);
+router.get('/api/v1/businesses?location', Business.getAllBusinesses);
+router.get('/api/v1/businesses?category', Business.getAllBusinesses);
 
 
 module.exports = router;
